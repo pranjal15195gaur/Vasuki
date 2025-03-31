b5adccc31aff6db27a84fc23080158cc80f25bd5
Ruchit Jagodara <ruchit.jagodara@iitgn.ac.in>
2025-03-31 22:10:13 +0530

    Added the updated lexer

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -57,7 +57,9 @@ def lex(s: str) -> Iterator[Token]:
         elif s[i].isalpha():
             t = s[i]
             i += 1
-            while i < len(s) and s[i].isalpha():
+            while i < len(s) and s[i].isalpha() or s[i].isdigit() or s[i] == '_':
+                if s[i].isdigit() and t[-1] == '_':
+                    raise ValueError("Invalid identifier token found :- {}".format(t))
                 t += s[i]
                 i += 1
             match t:
--- compiler/problems_by_TA.py
+++ compiler/problems_by_TA.py
@@ -4,23 +4,29 @@ from parser import parse, ParseError
 def problem_by_TA():
     # Problem statement: Write the multiplication table of 17 till 15 terms
     code = """
-            def f(){
-                return 1;
-            };
+            def f(g){
+                var x = 17;
+                var y = 5;
 
-            def foo(){
-                print(f());
-            };
-
-            def bar(){
-                def f(){
-                    return 10;
+                def s(){
+                    return x  + y+ g();
                 };
 
-                foo();
+                return s;
             };
 
-            bar();
+            def const42(){
+                return 42;
+            };
+
+            def const84(){
+                return 84;
+            };
+
+            var s1 = f(const42);
+            var s2 = f(const84);
+            print(s2());
+            print(s1());
            """
     try:
         ast = parse(code)
